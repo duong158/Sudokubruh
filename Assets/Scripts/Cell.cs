881385b6ee2a5734250a9cabb58b25ccdfc6cc41
duong158 <translateman7@gmail.com>
2024-12-01 22:06:39 +0700

    Initial commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,132 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+using TMPro;
+
+public class Cell : MonoBehaviour
+{
+    [HideInInspector] public int Value;
+    [HideInInspector] public int Row;
+    [HideInInspector] public int Col;
+    [HideInInspector] public bool IsLocked;
+    [HideInInspector] public bool IsIncorrect;
+
+    [SerializeField] private SpriteRenderer _bgSprite;
+    [SerializeField] private TMP_Text _valueText;
+    [Space]
+    [Header("Start")]
+    [SerializeField] private Color _startUnlockedCellColor;
+    [SerializeField] private Color _startUnlockedTextColor;
+    [SerializeField] private Color _startLockedCellColor;
+    [SerializeField] private Color _startLockedTextColor;
+    [Space]
+    [Header("Highlight")]
+    [SerializeField] private Color _highlightLockedTextColor;
+    [SerializeField] private Color _highlightUnlockedTextColor;
+    [SerializeField] private Color _highlightLockedCellColor;
+    [SerializeField] private Color _highlightUnlockedCellColor;
+    [SerializeField] private Color _highlightWrongCellColor;
+    [SerializeField] private Color _highlightWrongTextColor;
+    [Space]
+    [Header("Selected")]
+    [SerializeField] private Color _selectedCellColor;
+    [SerializeField] private Color _selectedTextColor;
+    [SerializeField] private Color _selectedWrongTextColor;
+    [SerializeField] private Color _selectedWrongCellColor;
+    [Space]
+    [Header("Reset")]
+    [SerializeField] private Color _resetCellColor;
+    [SerializeField] private Color _resetTextColor;
+    [SerializeField] private Color _resetWrongCellColor;
+    [SerializeField] private Color _resetWrongTextColor;
+
+    public void Init(int value)
+    {
+        IsIncorrect = false;
+        Value = value;
+
+        if (value == 0)
+        {
+            IsLocked = false;
+            _bgSprite.color = _startUnlockedCellColor;
+            _valueText.color = _startUnlockedTextColor;
+            _valueText.text = "";
+
+        }
+        else 
+        { 
+            IsLocked = true; 
+            _bgSprite.color= _startLockedCellColor;
+            _valueText.color= _startLockedTextColor;
+            _valueText.text = Value.ToString();
+        }
+    }
+    public void Highlight()
+    {
+        if(IsLocked)
+        {
+            _bgSprite.color = _highlightLockedCellColor;
+            _valueText.color = _highlightLockedTextColor;
+        }
+        else
+        {
+            if (IsIncorrect)
+            {
+                _bgSprite.color = _highlightWrongCellColor;
+                _valueText.color = _highlightWrongTextColor;
+            }
+            else
+            {
+                _bgSprite.color = _highlightUnlockedCellColor;
+                _valueText.color = _highlightUnlockedTextColor;
+            }
+        }
+    }
+
+    public void Select()
+    {
+        if (IsIncorrect)
+        {
+            _bgSprite.color = _selectedWrongCellColor;
+            _valueText.color = _selectedWrongTextColor;
+        }
+        else
+        {
+            _bgSprite.color = _selectedCellColor;
+            _valueText.color = _selectedTextColor;
+        }
+    }
+    public void Reset()
+    {
+        if (IsLocked)
+        {
+            _bgSprite.color = _startLockedCellColor;
+            _valueText.color = _startLockedTextColor;
+        }
+        else
+        {
+            if (IsIncorrect)
+            {
+                _bgSprite.color = _resetWrongCellColor;
+                _valueText.color = _resetWrongTextColor;
+            }
+            else
+            {
+                _bgSprite.color = _resetCellColor;
+                _valueText.color = _resetTextColor;
+            }
+        }
+    }
+
+    public void UpdateValue(int value)
+    {
+        Value = value;
+        _valueText.text = Value == 0? "" : Value.ToString();
+    }
+
+    public void UpdateWin()
+    {
+        _bgSprite.color = _startLockedCellColor;
+        _valueText.color = _startLockedTextColor;
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private Vector3 _startPos;
+    [SerializeField] private float _offsetX, _offsetY;
+    [SerializeField] private SubGrid _subGridPrefab;
+    [SerializeField] private TMP_Text _levelText;
+
+    private bool hasGameFinished;
+    private Cell[,] cells;
+    private Cell selectedCell;
+
+    private const int GRID_SIZE = 9;
+    private const int SUBGRID_SIZE = 3;
+
+    private void Start()
+    {
+        hasGameFinished = false;
+        cells = new Cell[GRID_SIZE, GRID_SIZE];
+        selectedCell = null;
+        SpawnCells();
+
+        
+    }
+
+
+
+    private void SpawnCells()
+    {
+        int[,] puzzleGrid = Generator.GeneratePuzzle(Generator.DifficultyLevel.EASY);
+
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            Vector3 spawnPos = _startPos + i % 3 * _offsetX * Vector3.right + i / 3 * _offsetY * Vector3.up;
+            SubGrid subGrid = Instantiate(_subGridPrefab, spawnPos, Quaternion.identity);
+            List<Cell> subgridCells = subGrid.cells;
+            int startRow = (i / 3) * 3;
+            int startCol = (i % 3) * 3;
+            for (int j = 0; j < GRID_SIZE; j++)
+            {
+                subgridCells[j].Row = startRow + j / 3;
+                subgridCells[j].Col = startCol + j % 3;
+                int cellValue = puzzleGrid[subgridCells[j].Row, subgridCells[j].Col];
+                subgridCells[j].Init(cellValue);
+                cells[subgridCells[j].Row, subgridCells[j].Col] = subgridCells[j];
+            }
+        }
+    }
+
+    private void GoToNextLevel()
+    {
+        RestartGame();
+    }
+
+    private void Update()
+    {
+        if(hasGameFinished || !Input.GetMouseButton(0)) { return; }
+
+        Debug.Log("Clicked");
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos2D = new Vector2(mousePos.x, mousePos.y);
+        RaycastHit2D hit = Physics2D.Raycast(mousePos2D, Vector2.zero);
+        Cell tempCell;
+        if(hit && hit.collider.gameObject.TryGetComponent(out tempCell) && tempCell != selectedCell)
+        {
+            ResetGrid();
+            selectedCell = tempCell;
+            Highlight();
+        }
+
+        
+    }
+
+    private void ResetGrid()
+    {
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            for (int j = 0; j < GRID_SIZE; j++)
+            {
+                cells[i, j].Reset();
+            }
+        }
+    }
+
+    public void UpdateCellValue(int value)
+    {
+        if (hasGameFinished || selectedCell == null) { return; }
+        if (!selectedCell.IsLocked)
+            selectedCell.UpdateValue(value);
+        Highlight();
+        CheckWin();
+    }
+
+    private void CheckWin()
+    {
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            for (int j = 0; j < GRID_SIZE; j++)
+            {
+                if (cells[i, j].IsIncorrect || cells[i, j].Value == 0) return;
+            }
+            hasGameFinished = true;
+        }
+
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            for(int j = 0; j < GRID_SIZE;j++)
+            {
+                cells[i, j].UpdateWin();
+            }
+        }
+
+        Invoke("GoToNextLevel", 2f);
+        
+    }
+    private void Highlight()
+    {
+        for (int i =  0; i<GRID_SIZE; i++)
+        {
+            for (int j = 0;  j<GRID_SIZE; j++)
+            {
+                cells[i, j].IsIncorrect = !isValid(cells[i, j],cells);
+            }
+        }
+        int currentRow = selectedCell.Row;
+        int currentCol = selectedCell.Col;
+        int subGridRow = currentRow - currentRow % SUBGRID_SIZE;
+        int subGridCol = currentCol - currentCol % SUBGRID_SIZE;
+
+        for (int i = 0; i < GRID_SIZE; i++)
+        {
+            cells[i, currentCol].Highlight();
+            cells[currentRow, i].Highlight();
+            cells[subGridRow + i % 3, subGridCol + i / 3].Highlight();
+        }
+        //lên màu cho ô giống ô đc chọn
+        for (int i = 0;i < GRID_SIZE; i++)
+        {
+            for(int j = 0;j < GRID_SIZE; j++)
+            {
+                if (cells[i, j].Value == cells[currentRow, currentCol].Value && cells[i, j].IsLocked)
+                {
+                    cells[i, j].Select();
+                }
+            }
+        }
+        cells[currentRow, currentCol].Select();
+    }
+    private bool isValid(Cell cell, Cell[,] cells)
+    {
+        int row = cell.Row;
+        int col = cell.Col;
+        int value = cell.Value;
+
+        if (value == 0)
+        {
+            return true;
+        }
+
+        for (int i = 0;i<GRID_SIZE; i++)
+        {
+            if (cells[row, i].Value == value || cells[i,col].Value == value)
+            {
+                cell.Value = value;
+                return false;
+            }
+        }
+
+        int subGridRow = row - row % SUBGRID_SIZE;
+        int subGridCol = col - col % SUBGRID_SIZE;
+
+        for (int r = subGridRow; r < subGridRow + SUBGRID_SIZE; r++)
+        {
+            for (int c = subGridCol;c  < subGridCol + SUBGRID_SIZE; c++)
+            {
+                if (cells[r,c].Value == value)
+                {
+                    cell.Value = value;
+                    return false;
+                }
+            }
+        }
+
+        cell.Value = value;
+        return true;
+
+    }
+
+    public void RestartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+}
