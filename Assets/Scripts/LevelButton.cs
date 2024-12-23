758e25ea49d63006666df34db10dfc989742e93d
duong158 <translateman7@gmail.com>
2024-12-24 00:20:47 +0700

    thêm nhiều thứ

---
+++ Assets/Scripts/LevelButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelButton : MonoBehaviour
+{
+    public Button easyButton;      // Nút chọn độ dễ
+    public Button mediumButton;    // Nút chọn độ trung bình
+    public Button hardButton;      // Nút chọn độ khó
+
+    private void Start()
+    {
+        // Gán sự kiện nhấn nút
+        easyButton.onClick.AddListener(() => SelectDifficulty(Generator.DifficultyLevel.EASY));
+        mediumButton.onClick.AddListener(() => SelectDifficulty(Generator.DifficultyLevel.MEDIUM));
+        hardButton.onClick.AddListener(() => SelectDifficulty(Generator.DifficultyLevel.DIFFICULT));
+    }
+
+    private void SelectDifficulty(Generator.DifficultyLevel difficultyLevel)
+    {
+        // Lưu độ khó được chọn vào PlayerPrefs
+        PlayerPrefs.SetInt("SelectedDifficulty", (int)difficultyLevel);
+
+        // Chuyển sang scene game
+        UnityEngine.SceneManagement.SceneManager.LoadScene("SampleScene");
+    }
+}
+
+
---
+++ Assets/Scripts/Music.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.Audio;
+
+public class Music : MonoBehaviour
+{
+    private static Music instance;
+    private AudioSource audioSource;
+
+    
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject); // Không phá hủy đối tượng này khi load scene mới
+        }
+        else
+        {
+            Destroy(gameObject); // Xóa AudioManager mới nếu đã có một cái tồn tại
+        }
+    }
+    public void PauseMusic()
+    {
+        if (audioSource.isPlaying)
+        {
+            audioSource.Pause();
+        }
+        else
+        {
+            audioSource.Play();
+        }
+    }
+}
